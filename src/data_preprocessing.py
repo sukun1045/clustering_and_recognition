92c01c413fb3e8b7e6645f0ebb5c539ac8f953f2
Kun Su <sukun1045@gmail.com>
2019-08-13 13:41:27 -0700

    Add files via upload

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -7,10 +7,7 @@ def define_actions(data_dir_train,num_actions):
 		actions_candidate = []
 		for fn in os.listdir(data_dir_train):
 			actions_candidate.append(fn)
-		if unique:
-			return random.sample(actions_candidate,k=num_actions)
-		else:
-			return random.choices(actions_candidate,k=num_actions)
+		return random.sample(actions_candidate,k=num_actions)
 
 def readCSVasFloat(filename):
     returnArray = []
@@ -34,27 +31,40 @@ def normalization_stats(all_data):
 
 class data_prep(object):
 	"""docstring for data_preprocessing"""
-	def __init__(self, dataset,num_actions,unique):
+	def __init__(self, dataset,num_actions,total_num_actions,actions = "random"):
+		self.total_num_actions = total_num_actions
 		if dataset=="cmu":
 			data_dir = ("./data/cmu_mocap")
+			data_dir_train = os.path.normpath(os.path.join(data_dir,"train"))
+		elif dataset=='h3.6m':
+			data_dir = ("./data/h3.6m/dataset")
+			data_dir_train = data_dir
 		
-		data_dir_train = os.path.normpath(os.path.join(data_dir,"train"))
 		print("the training directory is: {0}".format(data_dir_train))
 		self.norm_trainData = {}
 		self.norm_testData = {}
-		self.actions = define_actions(data_dir_train,num_actions,unique)
-		print("the actions sequences are the following: {0}".format(self.actions))
-		self.complete_train,self.trainData = self.load_data(data_dir_train)
+		
+		if actions!="random":
+			self.actions = actions
+		else:
+			self.actions = define_actions(data_dir_train,num_actions)
+		print("the unique type actions are: {0}".format(self.actions))
+		self.complete_train,self.trainData = self.load_cmu_data(data_dir_train)
 		self.data_mean,self.data_std,self.dim_ignore,self.dim_use = normalization_stats(self.complete_train)
 		self.norm_complete_train = self.normalize_data()
 
-	def load_data(self,path_to_dataset):
+	def load_cmu_data(self,path_to_dataset):
 		complete = []
 		Data = {}
-		nactions = len(self.actions)
 		print("load the following action data:")
-		for i in range(nactions):
-			action = self.actions[i]
+		for i in range(self.total_num_actions):
+			# make sure every unique action has at least 1 sequence.
+			if i < len(self.actions):
+				action = self.actions[i]
+			# otherwise, randomly pick
+			else:
+				action = random.choices(self.actions,k=1)[0]
+			#randomly choose action sequence from dataset
 			sub_path = '{}/{}'.format(path_to_dataset,action)
 			count = 0
 			for fn in os.listdir(sub_path):
@@ -65,15 +75,42 @@ class data_prep(object):
 			else:
 				j = np.random.randint(1,count+1)
 				filename = '{0}/{1}_{2}.txt'.format(sub_path,action,j)
+			#print the action sequence we chose
 			print(filename)
 			action_sequence = readCSVasFloat(filename)
 			r,c = action_sequence.shape
 			Data[(action,j)]=action_sequence
-
 			if len(complete)==0:
 				complete = copy.deepcopy(action_sequence)
 			else:
 				complete = np.append(complete,action_sequence,axis=0)
+		return complete,Data
+
+	def load_hm_data(self,path_to_dataset):
+		complete = []
+		Data = {}
+		print("load the following action data:")
+		for i in range(self.total_num_actions):
+			# make sure every unique action has at least 1 sequence.
+			if i < len(self.actions):
+				action = self.actions[i]
+			# otherwise, randomly pick
+			else:
+				action = random.choices(self.actions,k=1)[0]
+			#randomly choose action sequence from dataset
+			subjects = [1,5,6,7,8,9,11]
+			subj = random.choices(subjects,k=1)[0]
+			subact = np.random.randint(1,3)
+			filename = '{0}/S{1}/{2}_{3}.txt'.format(path_to_dataset,subj,action,subact)
+			print(filename)
+			action_sequence = readCSVasFloat(filename)
+			r,c = action_sequence.shape
+			even_list = range(0,r,2)
+			Data[(subj,action,subact)] = action_sequence[even_list,:]
+			if len(complete)==0:
+				complete = copy.deepcopy(action_sequence[even_list,:])
+			else:
+				complete = np.append(complete,action_sequence[even_list,:],axis=0)
 		return complete,Data
 
 	def normalize_data(self):
